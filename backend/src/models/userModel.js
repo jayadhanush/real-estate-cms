6fd6778e68dbd0787f1d050c520a35200f16b356
jayadhanush <jayadhanushravichandran@gmail.com>
2025-05-01 20:19:01 +0530

    userModel

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:["admin","user"],
+        default:"user"
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+})
