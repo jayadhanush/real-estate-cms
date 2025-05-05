dbd7391e24d4878bd6bfd73ba298e13fab5770ca
jayadhanush <jayadhanushravichandran@gmail.com>
2025-05-05 11:50:17 +0530

    added client

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const app = require('./src/app');
-
+const dotenv = require('dotenv');
 dotenv.config();
 
 const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/realEstate';
